028c8ce049fc636d4520e8feb3b3090655c9a5ae
DaPonce <danielponcegranado@gmail.com>
2020-12-03 23:07:56 +0100

    Todos los viernes a las 22:00 en twitch.tv/lucasitos

---
+++ AdventOfCode2020/Dia03/DaPonce/E2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace E2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = System.IO.File.ReadAllLines("input.txt");
+
+            int totalBarbol;
+
+            totalBarbol = barbolCount(1, 1, lines);
+            totalBarbol = totalBarbol * barbolCount(3, 1, lines);
+            totalBarbol = totalBarbol * barbolCount(5, 1, lines);
+            totalBarbol = totalBarbol * barbolCount(7, 1, lines);
+            totalBarbol = totalBarbol * barbolCount(1, 2, lines);
+
+            Console.WriteLine("Total: " + totalBarbol);
+        }
+
+        static int barbolCount(int xMove, int yMove, string[] lines)
+        {
+            int barbol = 0;
+            int x = xMove;
+            int linesLength = lines[0].Length;
+
+            for (int y = yMove; y < lines.Length; y += yMove)
+            {
+                // if (yMove > 1) Console.WriteLine(y + " + " + yMove + " < " + (lines.Length - (yMove - 1)));
+
+                if (lines[y][x] == '#')
+                {
+                    barbol++;
+                }
+
+                x = (x + xMove) % linesLength;
+            }
+            Console.WriteLine(xMove + "x " + " " + yMove + "y " + barbol);
+            return barbol;
+        }
+    }
+}
