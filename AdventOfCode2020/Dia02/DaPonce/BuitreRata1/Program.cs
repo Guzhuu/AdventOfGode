3b6e7ddd34eed2b6496810c6720d9ae8e00bd728
DaPonce <danielponcegranado@gmail.com>
2020-12-03 21:19:57 +0100

    Buitre ratita

---
+++ AdventOfCode2020/Dia02/DaPonce/BuitreRata1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace BuitreRata1
+{
+    class Program
+    {
+        static int correctPasswords = 0;
+        static void Main(string[] args)
+        {
+            string[] lines = System.IO.File.ReadAllLines("input.txt");
+
+            foreach (string line in lines)
+            {
+                string[] lineParts = line.Split(" ");
+                string[] ruleParts = lineParts[0].Split("-");
+                char lookingFor = lineParts[1][0];
+                int min = int.Parse(ruleParts[0]);
+                int max = int.Parse(ruleParts[1]);
+
+                int rep = 0;
+                foreach (char c in lineParts[2].ToCharArray())
+                {
+                    if (c == lookingFor) rep++;
+                }
+
+                if (rep >= min && rep <= max)
+                {
+                    correctPasswords++;
+                }
+            }
+            Console.WriteLine(correctPasswords);
+        }
+    }
+}
---
+++ AdventOfCode2020/Dia02/DaPonce/BuitreRata2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace BuitreRata2
+{
+    class Program
+    {
+        static int correctPasswords = 0;
+        static void Main(string[] args)
+        {
+            string[] lines = System.IO.File.ReadAllLines("input.txt");
+
+            foreach (string line in lines)
+            {
+                string[] lineParts = line.Split(" ");
+                string[] ruleParts = lineParts[0].Split("-");
+                char lookingFor = lineParts[1][0];
+                int first = int.Parse(ruleParts[0]);
+                int second = int.Parse(ruleParts[1]);
+
+                int rep = 0;
+
+                if (lineParts[2][first - 1] == lookingFor ^ lineParts[2][second - 1] == lookingFor) correctPasswords++;
+            }
+            Console.WriteLine(correctPasswords);
+        }
+    }
+}
