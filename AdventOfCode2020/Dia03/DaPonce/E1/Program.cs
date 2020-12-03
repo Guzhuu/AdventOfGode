c503bb29a3372821966032fdbb72c8bb91220efe
DaPonce <danielponcegranado@gmail.com>
2020-12-03 22:20:21 +0100

    Follow me twitch.tv/lucasitos uwu

---
+++ AdventOfCode2020/Dia03/DaPonce/E1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace E1
+{
+    class Program
+    {
+        static int barbol = 0;
+        static void Main(string[] args)
+        {
+            string[] lines = System.IO.File.ReadAllLines("input.txt");
+            int linesLength = lines[0].Length;
+            int x = 3;
+
+            for (int y = 1; y < lines.Length; y++)
+            {
+                if (lines[y][x] == '#')
+                {
+                    barbol++;
+                }
+
+                x = (x + 3) % linesLength;
+            }
+            Console.WriteLine(barbol);
+        }
+    }
+}
