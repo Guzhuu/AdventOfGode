063e033660cadcee3fb760aa395314a1d4b856bc
Danimf99 <danijugon@hotmail.com>
2020-12-03 12:08:24 +0100

    Dia 3 hecho en pitón

---
+++ AdventOfCode2020/Dia03/Danimf/parte1.py
@@ -0,0 +1,20 @@
+def resolver(camino, movimientoX, movimientoY):
+    actualX = 0
+    actualY = 0
+    arboles = 0
+
+    while (actualY := actualY + movimientoY) < len(camino):
+        actualX = (actualX + movimientoX) % len(camino[actualY])
+
+        if camino[actualY][actualX] == '#':
+            arboles += 1
+    return arboles
+
+with open("input.txt", "r") as file:
+    global lineas
+
+    lineas = [linea.strip() for linea in file.readlines()]
+    file.close()
+
+arboles = resolver(lineas, 3, 1)
+print(arboles)
